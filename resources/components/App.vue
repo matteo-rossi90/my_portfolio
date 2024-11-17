cf24340cbe8ec0f6378af2288ce299dbdeaf23ef
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-11-17 13:48:55 +0100

    feat: add router Vue

--- resources/components/App.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <h1>Questo sarà il mio portfolio</h1>
-</template>
---
+++ resources/js/App.vue
@@ -0,0 +1,19 @@
+<script>
+import Header from '../js/partials/Header.vue';
+
+export default{
+    components:{
+        Header
+    }
+}
+</script>
+
+<template>
+    <Header/>
+    <main>
+        <router-view></router-view>
+    </main>
+</template>
+
+<style>
+</style>
