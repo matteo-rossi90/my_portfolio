0fc476876708261690e562327f5904eaf2b941ad
matteo-rossi90 <matteo.rossi1314@gmail.com>
2025-03-21 00:26:23 +0100

    feat: set first scheme of the portfolio

--- src/pages/Portfolio.vue
+++ src/pages/Portfolio.vue
@@ -6,10 +6,29 @@ export default {
 
 <template>
     <section>
-        Qui vanno i progetti
+        <div class="content-container">
+
+            <div class="row text-center">
+                <h1>I miei progetti</h1>
+                <p class="text-standard">Qui puoi dare un'occhiata ai miei progetti.<br>Clicca sul progetto che vuoi visualizzare per maggiori informazioni</p>
+            </div>
+
+            <div class="row">
+                <div class="col-12 col-lg-4">
+
+                </div>
+            </div>
+
+        </div>
     </section>
 </template>
 
 <style lang="scss">
+@use '../style/variables' as*;
+
+section{
+    position: relative;
+    padding-top: 4rem;
+}
     
 </style>
